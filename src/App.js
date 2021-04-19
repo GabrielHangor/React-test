443f06b065163feb916e07e390ed677c6e0b718d
HangorG <gaba93@yandex.ru>
2021-04-19 21:52:37 +0300

    input form and submit btn class component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import React, { Component } from 'react';
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentInput: '',
+      tasks: [],
+    };
 
-function App() {
-  return <div>
-    
-  </div>;
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.setState({ tasks: [...this.state.tasks, this.state.currentInput] });
+  }
+
+  handleChange(e) {
+    this.setState({ currentInput: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            value={this.state.currentInput}
+            onChange={this.handleChange}
+            type="text"
+          />
+          <button onClick={this.handleClick}>Click Me</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
